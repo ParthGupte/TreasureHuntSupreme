48980df330e14e2a0c8947da25913d75efac41bd
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2021-09-04 12:51:33 +0530

    deal with registration
    registration process registration

--- thehunt/models.py
+++ thehunt/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    fullname = models.CharField(max_length=50)
-    username = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=50, unique=True)
+    iiserid = models.TextField(unique=True)
+    username = models.CharField(max_length=10, primary_key=True)
     password = models.CharField(max_length=10)
+    lvl = models.IntegerField(default=1)
--- thehunt/views.py
+++ thehunt/views.py
@@ -1,24 +1,27 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from thehunt.models import *
 # Create your views here.
 
 def home(request):
-    return render(request,'login.html')
+    return render(request,'login.html',{"msg":""})
 
 def register(request):
+
     return render(request,'register.html')
 
+def process_registration(request):
+    return HttpResponseRedirect('')
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
-    if User.objects.exists(username=username):
-        objlist = User.objects.get(username=username)
-        if password == objlist[0][2]:
+    
+    objlist = User.objects.filter(username=username)
+    if objlist.exists():
+        if objlist.first()[password] == password:
             return render(request,'main.html')
         else:
-            return render(request,'login.html')
+            return render(request, 'login.html',{"msg":"Incorrect Username or Password"})
     else:
-        return render(request,'login.html')
+        return render(request, 'login.html',{"msg":"Incorrect Username or Password"})
     
-
-
